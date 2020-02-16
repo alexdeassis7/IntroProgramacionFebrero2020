1931aafb0acb9622f70d7b81bd60771574905827
Alex <alexdeassis7@gmail.com>
2020-02-16 06:47:07 -0300

    Clase 1 Hola Mundo
    Clase 1 Hola Mundo

---
+++ Clase1/src/com/utn/demostracion/AppPrincipal.java
@@ -0,0 +1,11 @@
+package com.utn.demostracion;
+
+public class AppPrincipal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo soy un mensaje desde la consola");
+	
+	}
+
+}
